515fbeb72a385c3a0afd06c5e43985ef3f1c18f6
lastname <telivalen@icloud.com>
2024-06-01 16:27:57 -0400

    permission modificated added

--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Publication
 from .serializers import PublicationSerializer  
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class PublicationList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
 
 class PublicationDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
